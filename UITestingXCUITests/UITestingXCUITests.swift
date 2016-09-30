7db3521e3e34a8f0a84521392bd2bd2f3d5d9f53
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2016-09-30 16:28:26 +0200

    Fix XCUI tests

--- UITestingXCUITests/UITestingXCUITests.swift
+++ UITestingXCUITests/UITestingXCUITests.swift
@@ -5,8 +5,6 @@
 //  Copyright © 2016 PSPDFKit GmbH. All rights reserved.
 //
 
-import PSPDFKit
-
 class UITestingXCUITests: PSPDFTestCase {
         
     override func setUp() {
@@ -17,21 +15,19 @@ class UITestingXCUITests: PSPDFTestCase {
     }
     
     func testSearch() {
-        let app = XCUIApplication()
         XCUIDevice.shared().orientation = .portrait
+        let app = XCUIApplication()
 
         app.navigationBars["UITestingComparison.PDFView"].buttons["Search"].tap()
         
         let searchDocumentSearchField = app.searchFields["Search Document"]
         searchDocumentSearchField.typeText("PSPDF")
 
-      //  app.tables["Search Results"].cells[0].tap()
-
+        app.tables["Search Results"].cells.element(boundBy: 0).tap()
     }
 
     func testAddPage() {
         XCUIDevice.shared().orientation = .portrait
-        
         let app = XCUIApplication()
         let uitestingcomparisonPdfviewNavigationBar = app.navigationBars["UITestingComparison.PDFView"]
         uitestingcomparisonPdfviewNavigationBar.buttons["Thumbnails"].tap()
@@ -46,39 +42,18 @@ class UITestingXCUITests: PSPDFTestCase {
         app2.tables.staticTexts["Commit"].tap()
         app.buttons["Done"].tap()
 
-        app.sheets.buttons["Save As…"].tap()
-        app.navigationBars["Save As…"].buttons["Save"].tap()
+        app.sheets.buttons["Save"].tap()
         app.collectionViews["Thumbnail Collection"].cells["Page 1"].tap()
     }
 
-    func testaddAndDeleteBookmark() {
-        let fileURL = Bundle.main.bundleURL.appendingPathComponent("PSPDFKit 6 QuickStart Guide.pdf")
-        let document = PSPDFDocument(url: fileURL)
-        document.uid = NSUUID().uuidString
-        if let bookmarkManager = document.bookmarkManager {
-            for bookmark in bookmarkManager.bookmarks {
-                bookmarkManager.removeBookmark(bookmark)
-            }
-        }
-
-        let configuration = PSPDFConfiguration() { builder in
-            builder.shouldAskForAnnotationUsername = false
-        }
-
-        let controller = PSPDFViewController(document:document, configuration:configuration)
-
-        controller.navigationItem.rightBarButtonItems = [controller.bookmarkButtonItem, controller.outlineButtonItem]
-
-        test(with: controller) {
-            let app = XCUIApplication()
-            let outlineButton = app.navigationBars["PSPDFKit 6 QuickStart Guide"].buttons["Outline"]
-            outlineButton.tap()
-            app.navigationBars["Outline"].buttons["Bookmarks"].tap()
+    func testAddAndDeleteBookmark() {
+        let app = XCUIApplication()
+        let outlineButton = app.navigationBars["PSPDFKit 6 QuickStart Guide"].buttons["Outline"]
+        outlineButton.tap()
+        app.navigationBars["Outline"].buttons["Bookmarks"].tap()
         
-            let tablesQuery = app.tables
-            let worldMapStaticText = tablesQuery.staticTexts["Page 1"].swipeLeft()
-        }
-
+        let tablesQuery = app.tables
+        let worldMapStaticText = tablesQuery.staticTexts["World Map"].swipeLeft()
     }
     
 }
