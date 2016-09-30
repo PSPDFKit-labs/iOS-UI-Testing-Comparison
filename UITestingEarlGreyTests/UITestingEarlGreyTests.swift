48c124ff6da104280a98d8187e6dda5650c050d3
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2016-09-30 15:02:39 +0200

    Add new test case for EarlGrey

--- UITestingEarlGreyTests/UITestingEarlGreyTests.swift
+++ UITestingEarlGreyTests/UITestingEarlGreyTests.swift
@@ -9,10 +9,49 @@
 //  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
 //  This notice may not be removed from this file.
 //
+import PSPDFKit
+
 class UITestingEarlGreyTests: PSPDFTestCase {
     
     func testEarlGrey() {
         EarlGrey().selectElement(with: grey_accessibilityLabel("Thumbnails")).assert(with: grey_sufficientlyVisible()).perform(grey_tap())
+    }
+
+    func testAddAndDeleteBookmark() {
+        speed = .normal
+        let fileURL = Bundle.main.bundleURL.appendingPathComponent("PSPDFKit 6 QuickStart Guide.pdf")
+        let document = PSPDFDocument(url: fileURL)
+        document.uid = NSUUID().uuidString
+        if let bookmarkManager = document.bookmarkManager {
+            for bookmark in bookmarkManager.bookmarks {
+                bookmarkManager.removeBookmark(bookmark)
+            }
+        }
+
+        let configuration = PSPDFConfiguration() { builder in
+            builder.shouldAskForAnnotationUsername = false
+        }
+
+        let controller = PSPDFViewController(document:document, configuration:configuration)
+
+        controller.navigationItem.rightBarButtonItems = [controller.bookmarkButtonItem, controller.outlineButtonItem]
+
+        test(with: controller) {
+            XCTAssertEqual(controller.document!.bookmarks.count, 0)
+
+            EarlGrey().selectElement(with: grey_accessibilityLabel("Bookmarks")).assert(with: grey_sufficientlyVisible()).perform(grey_tap())
+            XCTAssertEqual(controller.document!.bookmarks.count, 1)
+
+            EarlGrey().selectElement(with: grey_accessibilityLabel("Outline")).assert(with: grey_sufficientlyVisible()).perform(grey_tap())
+            EarlGrey().selectElement(with: grey_text("Bookmarks")).perform(grey_tap())
+
+            EarlGrey().selectElement(with: grey_accessibilityLabel("Page 1")).assert(with: grey_sufficientlyVisible()).perform(grey_swipeSlowInDirection(.left))
+            EarlGrey().selectElement(with: grey_text("Delete")).assert(with: grey_sufficientlyVisible()).perform(grey_tap())
+
+            EarlGrey().selectElement(with: grey_accessibilityLabel("No Bookmarks")).assert(with: grey_sufficientlyVisible())
+
+            XCTAssertEqual(controller.document!.bookmarks.count, 0)
+        }
     }
     
 }
